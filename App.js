654a8ea4f2840f603f659c1e166185b9a15deeee
Windy Zhang <windy.zhang777@hotmail.com>
2023-07-26 20:03:49 -0500

    fixed fonts on app

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { useFonts } from "expo-font";
+import { StyleSheet } from "react-native";
+import HomeDrawer from "./routes/homeDrawer";
+
+const FONTS = {
+  "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+  "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded] = useFonts(FONTS);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  return <HomeDrawer />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
